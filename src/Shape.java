805871b020b60c45907b94be8f3d19036a6e8be7
aysmtl1 <ayssumtlu@gmail.com>
2025-04-25 11:57:28 +0300

    week9

--- src/Shape.java
+++ src/Shape.java
@@ -1,29 +1,16 @@
-public class Shape {
+public abstract class Shape {
 
     private String color = "white";
 
-    public Shape(){
+
+    public String Shape(){
 
     }
     public Shape(String color) {
         this.color = color;
     }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public double area(){
-        return 0;
-    }
-    public double perimeter(){
-        return 0;
-    }
-
+    public abstract double perimeter();
+    public abstract double area();
 
     @Override
     public String toString() {
